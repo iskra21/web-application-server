7b6b409aa93b5f24f33574ec32b94b581ebdc6aa
FSC <FSC@172.16.139.103>
2022-02-08 09:10:02 +0900

    1단계 테스트 반은 성공함

---
+++ src/main/java/util/HttpRequest.java
@@ -0,0 +1,75 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Strings;
+
+import webserver.RequestHandler;
+
+public class HttpRequest {
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+	
+	private String method;
+	private String url;
+	private String httpVer;
+	private Map<String,String> headers;
+	private Map<String,String> params;
+	
+	public HttpRequest(InputStream in) throws IOException {
+		BufferedReader ins = new BufferedReader(new InputStreamReader(in));
+		
+		String line = ins.readLine();
+		if (line == null) {
+			return;
+		}
+		
+		String[] tokens = line.split(" ");
+		this.method = tokens[0];
+		this.url = tokens[1];
+		this.httpVer = tokens[2];
+		
+    	this.headers = new HashMap<String,String>();
+    	while(!Strings.isNullOrEmpty(line = ins.readLine())) {
+    		HttpRequestUtils.Pair pair = HttpRequestUtils.parseHeader(line);
+    		headers.put(pair.getKey(), pair.getValue());
+    	}
+    	
+    	int index = this.url.indexOf('?');
+    	if (index != -1) {
+    		line = url.substring(index+1); // 이제 line에는 Query가 담기게 됨
+    		this.url = url.substring(0, index);
+    	}
+    	
+    	// 컨텐츠바디 읽어서 파싱하기
+    	if (!Strings.isNullOrEmpty(line)) {
+    		this.params = HttpRequestUtils.parseQueryString(line);    		
+    	} else if ("POST".equals(this.method) && !Strings.isNullOrEmpty(line = getHeader("Content-Length"))) {
+    		line = IOUtils.readData(ins, Integer.parseInt(line));
+    		this.params = HttpRequestUtils.parseQueryString(line);
+    	}
+	}
+
+	public String getHeader(String key) {
+		return this.headers.get(key);
+	}
+	
+	public String getParameter(String param) {
+		return this.params.get(param);
+	}
+
+	public String getMethod() {
+		return this.method;
+	}
+	
+	public String getUrl() {
+		return this.url;
+	}
+}
---
+++ src/test/java/util/HttpRequestTest.java
@@ -0,0 +1,25 @@
+package util;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.Test;
+
+public class HttpRequestTest {
+	private String testDir = ".\\src\\test\\resources\\";
+	
+	@Test
+	public void request_GET() throws IOException {
+		InputStream in = new FileInputStream(new File(testDir + "Http_GET.txt"));
+		HttpRequest request= new HttpRequest(in);
+		
+		assertEquals("GET", request.getMethod());
+		assertEquals("/user/create", request.getUrl());
+		assertEquals("keep-alive", request.getHeader("Connection"));
+		assertEquals("javajigi", request.getParameter("userId"));
+	}
+}
